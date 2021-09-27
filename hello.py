cd1aecffcaaaa1998582001abc80db179bc88d11
Nish <nishbpatel@yahoo.com>
2021-09-26 21:27:58 -0400

    Activity 4 Part 1

--- hello.py
+++ hello.py
@@ -3,14 +3,29 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'password'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html",current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old = session.get('name')
+        if old != form.name.data:
+            flash("Looks like you've changed your name!")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -18,4 +33,3 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
